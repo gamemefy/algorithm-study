ec507278cee3c08089e642e4b61bad754ab73520
gamemefy <megamemefy@gmail.com>
2022-05-04 18:29:47 +0900

    20220504 "etc" 디렉토리 생성

---
+++ etc/에라토스테네스의_체.py
@@ -0,0 +1,10 @@
+# 2 ~ n까지 수 중 n의 제곱근까지의 자연수 중 소수의 배수를 제거하고, 그 소수들과 나머지 수를 합하면 
+# 1 ~ n까지 모든 소수임
+import math
+
+def is_prime(x):
+    for i in range(2, math.ceil(math.sqrt(x)):
+        if x % i == 0:
+            return False
+    return True   
+    
