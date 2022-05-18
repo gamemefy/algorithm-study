eb25575b57a9941051fb49424934f2b685986afa
IDH <megamemefy@gmail.com>
2022-05-19 00:20:46 +0900

    20220519 주석수정

--- programmers/주식가격.py
+++ programmers/주식가격.py
@@ -7,7 +7,7 @@ def solution(prices):
         time = 0
         for q in queue:
             time += 1 # 일단 다음 번으로 넘어가면 1을 세어 줘야 함
-            if price > q: #
+            if price > q: 
                 break
             
         answer += [time] # answer.append(time)
